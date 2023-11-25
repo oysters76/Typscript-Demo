66e20d73d8d701f1f4018270351c6e0fb16f8367
Chirath <chirath.nishshanka@gmail.com>
2023-11-25 19:48:55 +0530

    Composition in TS

--- test.js
+++ test.js
@@ -5,3 +5,32 @@ function performAction(event) {
     liTag.innerHTML = val;
     (_a = document.querySelector(".list")) === null || _a === void 0 ? void 0 : _a.appendChild(liTag);
 }
+/* Composition in TS */
+var ImageEntity = /** @class */ (function () {
+    function ImageEntity(name) {
+        this.name = "";
+        this.name = name;
+    }
+    ImageEntity.prototype.resize = function (factor) { };
+    ImageEntity.prototype.remap = function (factor) { };
+    ImageEntity.prototype.rescale = function (factor) { };
+    return ImageEntity;
+}());
+var BitMapImage = /** @class */ (function () {
+    function BitMapImage(image) {
+        this.image = image;
+    }
+    BitMapImage.prototype.load = function (filename) {
+        throw new Error("Method not implemented.");
+    };
+    BitMapImage.prototype.save = function (filename) {
+        throw new Error("Method not implemented.");
+    };
+    return BitMapImage;
+}());
+var image = new ImageEntity("name");
+image.resize(5);
+image.remap(2);
+image.rescale(1);
+var bitMap = new BitMapImage(image);
+bitMap.save("test");
--- test.ts
+++ test.ts
@@ -4,3 +4,46 @@ function performAction(event:Event){
   liTag.innerHTML = val; 
   document.querySelector(".list")?.appendChild(liTag);
 }
+
+/* Composition in TS */
+class ImageEntity {
+  name:string = ""; 
+  
+  constructor (name:string){
+    this.name = name;
+  }
+
+  resize(factor:number){}
+  remap(factor:number){}
+  rescale(factor:number){}
+
+}
+
+interface ImageFile {
+  load(filename:string) : boolean; 
+  save(filename:string) : boolean;
+}
+
+class BitMapImage implements ImageFile{
+
+  image : ImageEntity;
+  
+  constructor(image : ImageEntity){
+    this.image = image;
+  }
+
+  load(filename: string): boolean {
+    throw new Error("Method not implemented.");
+  }
+  save(filename: string): boolean {
+    throw new Error("Method not implemented.");
+  }
+
+}
+
+let image = new ImageEntity("name"); 
+image.resize(5); 
+image.remap(2); 
+image.rescale(1); 
+let bitMap = new BitMapImage(image);  
+bitMap.save("test");
